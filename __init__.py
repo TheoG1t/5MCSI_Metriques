66d6b1ead3447b033572a4a83c51da541de55032
TheoBuis <159757744+TheoG1t@users.noreply.github.com>
2024-02-12 11:36:34 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,13 +4,12 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
+
 app = Flask(__name__)
 
 @app.route("/commits/")
 def moncommits():
     return render_template('commits.html')
-        return render_template("commits.html")
 
 @app.route("/histogramme/")
 def monhistogramme():
@@ -41,4 +40,47 @@ def hello_world():
     return render_template('hello.html') #Comm4
   
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import render_template
+from flask import json
+from datetime import datetime
+from urllib.request import urlopen
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/commits/")
+def moncommits():
+    return render_template('commits.html')
+
+@app.route("/histogramme/")
+def monhistogramme():
+    return render_template("histogramme.html")
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+
+@app.route('/paris/')
+def meteo():
+    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+
+@app.route("/contact/")
+def MaPremiereAPI():
+    return render_template("contact.html")
+                                                                                                                                       
+@app.route('/')
+def hello_world():
+    return render_template('hello.html') #Comm4
+  
+if __name__ == "__main__":
+    app.run(debug=True)
