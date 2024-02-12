71e206885436696c97a94dd0f0ca6cccfc6adbe2
TheoBuis <159757744+TheoG1t@users.noreply.github.com>
2024-02-12 11:24:15 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,11 +7,9 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)
 
-@app.route('/extract-minutes/<date_string>')
-def extract_minutes(date_string):
-        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-        minutes = date_object.minute
-        return jsonify({'minutes': minutes})
+@app.route("/commits/")
+def moncommits():
+    return render_template('commits.html')
         return render_template("commits.html")
 
 @app.route("/histogramme/")
